d2f353a7743932053735975f79b068831ca7091e
jtsaito <jahn.saito@gmail.com>
2016-04-12 21:07:35 +0200

    Add empty unit test

---
+++ test/test.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestLambda(unittest.TestCase):
+    "Test Lambda class"
+
+    def setUp(self):
+        return True
+
+    def test_lambda(self):
+        """Always succeeds"""
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
